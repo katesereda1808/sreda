aa6ac28e1fd698a3bbc1e3c3ae654a1a4f5da2c4
katesereda1808 <katesereda22@gmail.com>
2022-05-17 16:31:14 +0300

    slider added

--- script.js
+++ script.js
@@ -11,13 +11,6 @@ let blocks = document.querySelectorAll('.block');
 for (let block of blocks) {
   observer.observe(block);
 }
-let dots = document.querySelector('.dots');
-for (let i = 0; i < 8; i++) {
-  let div = document.createElement('div');
-div.className = 'dot';
-dots.appendChild(div);
-}
-dots.firstElementChild.classList.add('dot_active');
 
 let mobile_menu = document.querySelector('.nav_mobile');
 let close_menu = document.querySelector('.burger-close');
@@ -91,3 +84,125 @@ function onSubmit(e){
     };
   };
 };
+
+let slideIndex = 0;
+let slider_content = document.querySelector('.slider__content');
+
+let dot_container = document.querySelector('.dots');
+let dots = document.getElementsByClassName("dot");
+console.log(dots);
+
+function addDots() {
+  dot_container.innerHTML = '';
+  for (let i = 0; i < slider_content.children.length; i++) {
+    // console.log(slider_content.children);
+    let div = document.createElement('div');
+    div.className = 'dot';
+    dot_container.appendChild(div);
+  };
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', 
+    ()=>showSlides(i)
+    )
+  }
+};
+addDots();
+
+let slides = document.getElementsByClassName("slide");
+
+function showSlides(n){
+  if (n > slides.length) {n = 0}    
+  if (n < 0) {n = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    // slides[i].style.display = "none";
+
+  slides[i].classList.remove('shown');
+  slides[i].classList.add('hidden')
+  console.log('hide')
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dot_active", "");
+  }
+  // slides[n].style.display = "block";
+
+  slides[n].classList.remove('hidden');
+  slides[n].classList.add('shown');
+  dots[n].className += " dot_active";
+}
+
+let chevron_left = document.querySelector('#chevron_left')
+let chevron_right = document.querySelector('#chevron_right')
+console.log(chevron_left)
+
+function findSlide() {
+  let shown = slider_content.querySelector('.shown');
+  slidesArr = Array.from(slides);
+  console.log(slidesArr.indexOf(shown));
+  return(slidesArr.indexOf(shown));
+}
+
+chevron_left.addEventListener('click', ()=>showSlides(findSlide()-1));
+chevron_right.addEventListener('click', ()=>showSlides(findSlide()+1));
+
+// 900px
+
+let floor19 = document.querySelector('.floor19');
+console.log(floor19)
+let floor20 = document.querySelector('.floor20');
+
+function getWidth(){
+  let width = document.documentElement.clientWidth;
+  if(width<=900){
+    showSlides(slideIndex);
+
+
+    let floor19 = document.querySelector('.floor19_mobile');
+    let floor20 = document.querySelector('.floor20_mobile');
+    floor19.addEventListener('click', function() {
+      addASlide(slider_content, arr19);
+    });
+    floor20.addEventListener('click', function() {
+      addASlide(slider_content, arr20);
+    });
+  } else {
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "block";
+    }
+  }
+};
+getWidth();
+window.addEventListener('resize', getWidth);
+
+let arr19 = ['images/slider_1.jpg', 'images/slider_2.jpg', 'images/slider_3.jpg'];
+let arr20 = ['images/1132 (61).JPG', 'images/1132 (45).JPG', 'images/1132 (37).JPG', 'images/1132 (25).JPG', 'images/1132 (18).JPG', 'images/1132 (13).JPG', 'images/1132 (12).JPG'];
+
+
+function addASlide(place, arr) {
+  place.innerHTML='';
+  for (let i = 0; i < arr.length; i++) {
+    place.innerHTML+=`
+    <div class="slide">
+      <img class="img" src="${arr[i]}">
+    </div>`
+  };
+  addDots();
+}
+floor19.addEventListener('click', function() {
+  addASlide(slider_content, arr19);
+});
+floor20.addEventListener('click', function() {
+  addASlide(slider_content, arr20);
+});
+
+let arrow_right = document.getElementById('arrow_right');
+let arrow_left = document.getElementById('arrow_left');
+function scrollSlides(n) {
+  n>0?
+  slider_content.scrollLeft+=300:
+  slider_content.scrollLeft-=300;
+}
+
+arrow_right.addEventListener('click', ()=>scrollSlides(1));
+arrow_left.addEventListener('click', ()=>scrollSlides(-1));
